57bea3100c73cf3d405174a465746c0b333ceacb
Eduardo <edubaik@gmail.com>
2018-11-30 20:15:31 -0800

    Add optimized version of Inflight Movie algorithm

--- MovieFlight.java
+++ MovieFlight.java
@@ -1,11 +1,16 @@
 package hackerrank;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class MovieFlight {
 	public static void main(String[] args) {
-		int[] movieLengths = {1, 6, 5, 8, 7};
-		canTwoMoviesFillFlight(movieLengths, 12);
+//		int[] movieLengths = {3, 8};
+		
+      System.out.println(canTwoMoviesFillFlightOptimized(new int[] {2, 4}, 6));
+//        final boolean result = canTwoMoviesFillFlight(new int[] {1, 2, 3, 4, 5, 6}, 7);
+
+//		System.out.println(canTwoMoviesFillFlightOptimized(movieLengths, 6));
 	}
 	
     public static boolean canTwoMoviesFillFlight(int[] movieLengths, int flightLength) {
@@ -30,5 +35,40 @@ public class MovieFlight {
         return false;
     }
 
+    public static boolean canTwoMoviesFillFlightOptimized(int[] movieLengths, int flightLength) {
+    	HashMap<Integer, Integer> map = new  HashMap<Integer, Integer>();
+    	
+    	for(int i = 0; i < movieLengths.length; i++) {
+    		Integer frequency = map.get(movieLengths[i]);
+    		
+    		if(frequency == null) {
+    			map.put(movieLengths[i], 1);
+    		} else {
+    			map.put(movieLengths[i], frequency + 1);
+    		}    	
+    	}
+    	
+    	for(int movie : movieLengths) {
+    		int possibleComplement = flightLength - movie;
+    		
+    		Integer frequency = map.get(possibleComplement);
+    		
+    		// found a counter part
+    		if(frequency !=  null) {
+    		    // check if there are more than 1 movie when they are of the same value;
+    			if(possibleComplement == movie) {
+    				if(frequency > 1) {
+    					return true;
+    				} else {
+    					continue;
+    				}
+    			} else {
+    				return true;
+    			}
+    		}
+    	}
+    	
+    	return false;
+    }
 
 }
