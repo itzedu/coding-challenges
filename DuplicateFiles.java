3f830f1c3164b3c513b1f471b05dd300491a33ff
Eduardo <edubaik@gmail.com>
2018-12-05 16:26:25 -0800

    Finish implementing identification of duplicate files

--- DuplicateFiles.java
+++ DuplicateFiles.java
@@ -2,48 +2,49 @@ package hackerrank;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class DuplicateFiles {
-	
+public class DuplicateFiles {	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         List<File> files = new ArrayList<File>();
 		listf("/Users/eduardobaik/Desktop/rec/", files);
-		findDuplicates(files);
-      
+		System.out.println(findDuplicates(files));
 	}
 
-    public static void findDuplicates(List<File> files) {
+    public static List<List<File>> findDuplicates(List<File> files) {
+    	// For this problem, we are going to make some assumptions
+    	// If the last modified timestamps of a file are the same, then they must be the same file
+
+    	List<List<File>> duplicates = new ArrayList<List<File>>();
+
     	HashMap<Long, List<File>> map = new HashMap<Long, List<File>>();
     	
     	for(File f : files) {
-    		try {
-				BasicFileAttributes attr = Files.readAttributes(f.toPath(), BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
-				Long fileSize = attr.size();
-				List<File> duplicateFiles = map.get(attr.size());
-				if(duplicateFiles == null) {
-					map.put(fileSize, new ArrayList<File>(Arrays.asList(f)));
-				} else {
-					duplicateFiles.add(f);
-					map.put(fileSize, duplicateFiles);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
+    		Long lastMod = f.lastModified();
+    		List<File> dup = map.get(lastMod);
+    		
+    		if(dup == null) {
+    			map.put(lastMod, new ArrayList<File>(Arrays.asList(f)));
+    		} else {
+    			dup.add(f);
+    		}
     	}
-    }
-    
-    public static void checkFileContents(List<File> files) {
-    	// check if the files in the array are duplicates or not
+    	
+    	for(List<File> dups : map.values()) {
+    		if(dups.size() > 1) {
+    			duplicates.add(dups);
+    		}
+    	}    	
+    	
+    	return duplicates;
     }
     
     public static List<File> listf(String directoryName, List<File> files) {
