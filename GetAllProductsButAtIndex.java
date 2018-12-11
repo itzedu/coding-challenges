53aeea8160ce16a469ab27bcc90c730e3f28335a
Eduardo <edubaik@gmail.com>
2018-12-10 19:33:50 -0800

    Finish O(n^2) solution for AllProductsExceptIndex

--- GetAllProductsButAtIndex.java
+++ GetAllProductsButAtIndex.java
@@ -1,17 +1,30 @@
 package hackerrank;
 
+import java.util.Arrays;
+
 public class GetAllProductsButAtIndex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		getProductsOfAllIntsExceptAtIndex(new int[] {1, 7, 3, 4});
 	}
 
     public static int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
-
-        // make an array of the products
+    	// make an array of the products
+    	// however we cannot use division for this problem
+    	int[] result = new int[intArray.length];
+    	
+    	for(int i = 0; i < intArray.length; i++) {
+    		int product = 1;
+    		for(int j = 0; j < intArray.length; j++) {
+    			if(j != i) {
+    				product *= intArray[j];
+    			}
+    		}
+    		result[i] = product;
+    	}
         
-
-        return new int[intArray.length];
+    	System.out.println(Arrays.toString(result));
+        return result;
     }
 }
