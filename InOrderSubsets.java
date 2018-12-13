55098f484cbdde86a48a2483d2d77910488b7006
Eduardo <edubaik@gmail.com>
2018-12-12 19:43:49 -0800

    Finish String In Order Subsets

--- InOrderSubsets.java
+++ InOrderSubsets.java
@@ -7,35 +7,19 @@ public class InOrderSubsets {
 	private static List<String> result = new ArrayList<String>();
 	
 	public static void main(String[] args) {
-		findSubsets("abc", "", "abc");
+		findSubsets("abc", "");
 		System.out.println("Result - " + result);
 
 	}
 	
-	public static void findSubsets(String str, String prefix, String suffix) {
-		System.out.println("Result - " + result);
-		System.out.println("Prefix - " + prefix);
-		System.out.println("Suffix - " + suffix);
-		System.out.println("****************************");
-		for(int i = 0; i < suffix.length(); i++) {
-			
-			if(prefix.equals("")) {				
-				prefix = str.substring(i, i + 1);
-				suffix = suffix.substring(1);
-			}
-			
-			String word = prefix + suffix;
-			System.out.println("Word - " + word);
+	public static void findSubsets(String str, String prefix) {
+	    int length = str.length();
+	    
+	    result.add(prefix);
 
-			if(word.equals(str)) {
-				result.add(prefix);
-			} else {
-				result.add(prefix + suffix.substring(i, i + 1));
-			}
-			findSubsets("abc", prefix, suffix);
-			
+	    for (int i = 0 ; i < length; i++) {
+	    	findSubsets(str.substring(i+1), prefix + str.substring(i, i + 1));
 		}
 			  
 	}
-
 }
