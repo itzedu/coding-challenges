ad3bb96a8af9775bd678a111fe47f3e433e3898b
Eduardo <edubaik@gmail.com>
2018-12-18 09:49:49 -0800

    Start Graph Lesson

---
+++ GraphLesson.java
@@ -0,0 +1,12 @@
+package hackerrank;
+
+public class GraphLesson {
+	public static void main(String[] args) {
+		// edge list
+		// A list of all the edges in the graph: node 0 is connected to node 1, node 1 is connected to node 2, node 1 is connected to node 3, node 2 is connected to node 3
+		int[][] graph = {{0, 1}, {1, 2}, {1, 3}, {2, 3}};
+
+
+	}
+
+}
