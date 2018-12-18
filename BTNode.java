4a5c1a44ca303ecf16ec4cf39a4407aee1f28d78
Eduardo <edubaik@gmail.com>
2018-12-18 13:47:26 -0800

    Add BTNode class

---
+++ BTNode.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+public class BTNode {
+    public int value;
+    public BTNode left;
+    public BTNode right;
+
+    public BTNode(int value) {
+        this.value = value;
+    }
+
+    public BTNode insertLeft(int leftValue) {
+        this.left = new BTNode(leftValue);
+        return this.left;
+    }
+
+    public BTNode insertRight(int rightValue) {
+        this.right = new BTNode(rightValue);
+        return this.right;
+    }
+}
