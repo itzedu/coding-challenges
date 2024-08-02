fc06892544b67ac411231b522b588ce7836bfa00
itzedu <eduardo@numerade.com>
2024-08-02 16:43:18 -0700

    Update limit

--- leetcode-150/rotate-array.js
+++ leetcode-150/rotate-array.js
@@ -6,9 +6,16 @@
 var rotate = function (nums, k) {
   let result = nums.slice(nums.length - k);
 
-  for (let i = 0; i < k + 1; i++) {
+  let loopLimit = k;
+
+  if (k % 2 != 0) {
+    loopLimit = loopLimit + 1;
+  }
+
+  for (let i = 0; i < loopLimit; i++) {
     result.push(nums[i]);
   }
 
+  console.log(result);
   return result;
 };
