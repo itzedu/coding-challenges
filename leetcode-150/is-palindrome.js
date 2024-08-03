427f2c216a6a6ec994a6d0accc61d747d120d508
itzedu <eduardo@numerade.com>
2024-08-03 16:06:05 -0700

    Add is-palindrome

---
+++ leetcode-150/is-palindrome.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let start = 0;
+  let end = s.length - 1;
+
+  while (start < end) {
+    if (s[start] != s[end]) {
+      return false;
+    }
+
+    start++;
+    end--;
+  }
+
+  return true;
+};
